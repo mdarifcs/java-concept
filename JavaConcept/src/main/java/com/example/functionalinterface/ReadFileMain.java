b8fb3275ce368bf4d7c96d75154085e5c6ad3e8c
mohammad arif <mdarif.cs@gmail.com>
2019-07-07 23:44:20 +0530

    Read file in java 8 ways

---
+++ JavaConcept/src/main/java/com/example/functionalinterface/ReadFileMain.java
@@ -0,0 +1,21 @@
+package com.example.functionalinterface;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReadFileMain {
+	public static void main(String[] args) throws IOException {
+		List<Employee> employees = Files.lines(Paths.get("C:\\Users\\mohammad.arif\\Desktop\\employee.csv"))
+										.skip(1)
+										.map(line->{
+											String[] arr = line.split("\\,");
+											Employee emp = new Employee(arr[0], Integer.valueOf(arr[1]), Double.valueOf(arr[2]));
+											return emp;
+											})
+										.collect(Collectors.toList());
+		System.out.println(employees);
+	}
+}
