4353a29b5a175c9ffaa15cda6c2522e3f7f89370
mohammad arif <mdarif.cs@gmail.com>
2019-07-07 15:01:32 +0530

    Update Consumer Functional Interface

--- JavaConcept/src/main/java/com/example/functionalinterface/ConsumerDemo.java
+++ JavaConcept/src/main/java/com/example/functionalinterface/ConsumerDemo.java
@@ -2,9 +2,13 @@ package com.example.functionalinterface;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ConsumerDemo {
 	public static void main(String[] args) {
+		Consumer<String> consume = t -> System.out.println(t);
+		consume.accept("consumed");
+		
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 		
 		// foreach use consumer as argument (Consumer<? super Integer> action)
