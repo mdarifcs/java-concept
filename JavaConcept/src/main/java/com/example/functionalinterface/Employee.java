4659e80e8839b1be16e89485f4bb8f2c805d8e4f
mohammad arif <mdarif.cs@gmail.com>
2019-08-02 18:30:27 +0530

    Add Lombok to Employee

--- JavaConcept/src/main/java/com/example/functionalinterface/Employee.java
+++ JavaConcept/src/main/java/com/example/functionalinterface/Employee.java
@@ -1,34 +1,20 @@
 package com.example.functionalinterface;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Employee {
 	private String name;
 	private int age;
 	private double salary;
-	public Employee(String name, int age, double salary) {
-		this.name = name;
-		this.age = age;
-		this.salary = salary;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", age=" + age + ", salary=" + salary + "]";
-	}
 }
