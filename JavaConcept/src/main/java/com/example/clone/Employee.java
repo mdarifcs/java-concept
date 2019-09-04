63c15d1997706305f7f206fbe7c6bc4c575cdecf
mohammad arif <mdarif.cs@gmail.com>
2019-09-04 08:33:01 +0530

    Add lombak

--- JavaConcept/src/main/java/com/example/clone/Employee.java
+++ JavaConcept/src/main/java/com/example/clone/Employee.java
@@ -1,5 +1,12 @@
 package com.example.clone;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author mohammad.arif
  * So deep cloning requires satisfaction of following rules –
@@ -10,19 +17,16 @@ package com.example.clone;
  * of that member class and copy all its attributes one by one to new member class object.
  * This new member class object will be set in cloned object. 
  */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee implements Cloneable {
 	private int empId;
 	private String empName;
 	private Department department;
-
-	public Employee() {
-	}
-	
-	public Employee(int empId, String empName, Department department) {
-		this.empId = empId;
-		this.empName = empName;
-		this.department = department;
-	}
 
 	@Override
 	protected Employee clone() throws CloneNotSupportedException {
@@ -36,34 +40,5 @@ public class Employee implements Cloneable {
 				
 		cloned.setDepartment(dept);
 		return cloned;
-	}
-
-	public int getEmpId() {
-		return empId;
-	}
-
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public Department getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", department=" + department + "]";
 	}
 }
