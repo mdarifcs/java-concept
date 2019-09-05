968b5d2e7cbbf6fca8da97fbe67e6ec37e5d68af
mohammad arif <mdarif.cs@gmail.com>
2019-09-05 08:52:29 +0530

    Apply lombok to Department

--- JavaConcept/src/main/java/com/example/clone/Department.java
+++ JavaConcept/src/main/java/com/example/clone/Department.java
@@ -1,5 +1,12 @@
 package com.example.clone;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author mohammad.arif Implementing Cloneable interface and overriding clone
  *         methods in deparment class only will not create separate copy
@@ -7,38 +14,19 @@ package com.example.clone;
  *         To achieve deep copy (i.e. separate copy of department object) we
  *         have to modify clone object of employee class as well.
  */
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Department implements Cloneable {
 	private int id;
 	private String name;
-
-	public Department() {
-	}
-
-	
-	public Department(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
 
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		return super.clone();
 	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
